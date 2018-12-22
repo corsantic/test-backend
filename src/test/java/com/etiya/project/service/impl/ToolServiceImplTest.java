e906f2b1e6c4b70b3fc690196b94d27701f6c5f1
corsantic <aydofurkan25@gmail.com>
2018-12-22 12:45:22 +0300

    uml

---
+++ src/test/java/com/etiya/project/service/impl/ToolServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.etiya.project.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToolServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
